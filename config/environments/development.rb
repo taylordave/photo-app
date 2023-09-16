e34f5642215e0cf63a74e0abc19aa25f172bb07c
Dave Taylor <ddataylor@gmail.com>
2023-09-16 13:25:02 +0100

    Sending Email in Production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -67,4 +67,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: Rails.application.credentials.dig(:sendgrid, :username),
+  #   password: Rails.application.credentials.dig(:sendgrid, :password),
+  #   domain: Rails.application.credentials.dig(:sendgrid, :domain),
+  #   tls: true,
+  #   enable_starttls_auto: true
+  # }
 end
