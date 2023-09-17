79127a82aec30b9a56d8c7e16f7c6f7d3c690644
Dave Taylor <ddataylor@gmail.com>
2023-09-17 15:13:38 +0100

    Update sign up form for Credit Card payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,11 +3,11 @@ class Payment < ApplicationRecord
   belongs_to :user
 
   def self.month_options
-    Date::MONTHNAMES.compact.each_with_index.map{ |name, i| ["#{i-1} - #{name}", i - 1] }
+    Date::MONTHNAMES.compact.each_with_index.map{ |name, i| ["#{i + 1} - #{name}", i + 1] }
   end
 
   def self.year_options
-    (Date.today.year..(Date.today.year-10)).to_a
+    (Date.today.year..(Date.today.year+10)).to_a
   end
 
   def process_payment
